c5ca9f493d5b388eec1541df8f945b1a8a7b4cd3
Wandile <wasiteec025@student.wethinkcode.co.za>
2026-06-05 10:58:59 +0200

    chore: initial maven project generation

---
+++ src/main/java/com/portfolio/simulation/App.java
@@ -0,0 +1,13 @@
+package com.portfolio.simulation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
